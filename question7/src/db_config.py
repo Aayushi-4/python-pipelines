fdf40e931922bc6013b48e541684e3cc86d8d55f
Aayushi-4 <aayushi.gupta@kasmodigital.com>
2025-06-30 11:34:46 +0530

    Delete question7 directory

--- question7/src/db_config.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-def get_db_config(section):
-    config=configparser.ConfigParser()
-    config.read(r'C:\Users\Aayushi\Documents\python_tutorial\question7\config.ini')
-    print("Available sections: ", config.sections())
-    return config[section]
